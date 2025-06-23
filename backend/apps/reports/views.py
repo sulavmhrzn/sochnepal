af1290291075386f243c5271d2816666b38e0071
Sulav Maharjan <sulavmaharjan63@gmail.com>
2025-06-23 08:31:06 +0545

    feat(reports): add view to retrieve reports created by currently authenticated user

--- backend/apps/reports/views.py
+++ backend/apps/reports/views.py
@@ -1,7 +1,9 @@
 from django_filters import rest_framework as filters
+from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView
 from rest_framework.parsers import JSONParser, MultiPartParser
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import ReportFilter
@@ -22,6 +24,16 @@ class ReportViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    def my(self, request):
+        queryset = self.filter_queryset(
+            self.get_queryset().filter(created_by=request.user)
+        )
+        serializer = self.serializer_class(
+            queryset, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
 
 
 class CategoryView(ListAPIView):
