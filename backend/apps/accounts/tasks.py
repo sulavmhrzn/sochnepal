a454abf4a66bd920d287fa5773906699acd42c0a
Sulav Maharjan <sulavmaharjan63@gmail.com>
2025-07-06 10:20:05 +0545

    chore: use env variable for frontend_url key in settings

--- backend/apps/accounts/tasks.py
+++ backend/apps/accounts/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
@@ -15,7 +16,7 @@ def send_verification_email(user_pk):
     user_id = urlsafe_base64_encode(force_bytes(user.pk))
     context = {
         "first_name": user.first_name,
-        "url": f"http://localhost:3000/accounts/verify?uid={user_id}&token={token}",
+        "url": f"{settings.FRONTEND_URL}/verify-email?uid={user_id}&token={token}",
     }
 
     msg_html = render_to_string("verify_email.html", context)
