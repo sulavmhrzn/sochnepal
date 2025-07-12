d4232357542ec31085219828f17bd1754ad9d1d5
Sulav Maharjan <sulavmaharjan63@gmail.com>
2025-07-12 13:21:38 +0545

    feat: use toxicity predictor on comments app

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from .toxicity_detector import detector
 
 app = FastAPI()
 
+# origins = ["http://localhost:8000", "http://localhost"]
 
-class Content(BaseModel):
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Comment(BaseModel):
     id: int
     comment: str
+
+
+class CommentOut(Comment):
+    confidence: float
     is_toxic: bool | None = False
 
 
@@ -17,12 +33,18 @@ async def index():
     return {"msg": "fastapi service"}
 
 
-@app.post("/toxicity")
-async def toxicity(contents: list[Content]):
+@app.post("/predict-toxicity")
+async def predict_toxicity(comments: list[Comment]):
     results = []
-    for content in contents:
-        res = detector.detect_toxicity(content.comment)
-        content.is_toxic = bool(res[0])
-        results.append(content)
+    for comment in comments:
+        is_toxic, final_score, _ = detector.detect_toxicity(comment.comment)
+        results.append(
+            CommentOut(
+                id=comment.id,
+                comment=comment.comment,
+                confidence=final_score,
+                is_toxic=bool(is_toxic),
+            )
+        )
 
     return {"results": results}
--- backend/apps/comments/views.py
+++ backend/apps/comments/views.py
@@ -1,5 +1,9 @@
+import json
+
+import requests
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.permissions import IsCommentOwnerOrReadOnly, IsVerifiedUserOrReadOnly
@@ -29,7 +33,32 @@ class CommentViewSet(ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         get_object_or_404(Report, pk=self.kwargs["report_pk"])
-        return super().list(request, *args, **kwargs)
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        if len(queryset) > 0:
+            response = requests.post(
+                "http://ai-service:8001/predict-toxicity",
+                json.dumps(
+                    [
+                        {"id": value["id"], "comment": value["content"]}
+                        for value in queryset.values()
+                    ]
+                ),
+            )
+            results_by_id = {r["id"]: r for r in response.json()["results"]}
+            data = []
+
+            for comment in serializer.data:
+                result = results_by_id.get(comment["id"])
+                if result:
+                    comment["is_toxic"] = result["is_toxic"]
+                    comment["confidence"] = result["confidence"]
+                data.append(comment)
+
+            return Response(data)
+        else:
+            return Response(serializer.data)
+        # return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
